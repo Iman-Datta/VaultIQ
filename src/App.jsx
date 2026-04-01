fc381492e7f09bb4e4ace98556171ca230671010
Iman Datta <dattaiman56@gmail.com>
2026-04-01 21:50:51 +0530

    Intial commit and setup react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold bg-amber-200">
+      VaultIQ Dashboard
+    </div>
+  )
+}
+
+export default App
